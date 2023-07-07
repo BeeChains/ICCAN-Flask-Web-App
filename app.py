22d96fbd5c3c1e9989a94daf9df5f1ae0ab91ab2
xn--tbdhns/ | git.༕༞༑༙ <37987346+BeeChains@users.noreply.github.com>
2023-07-07 15:09:34 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+from iccan import ICCAN
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+# Get parameters from request
+color = request.form['color']
+texture = request.form['texture']
+pattern = request.form['pattern']
+
+# Generate camouflage design
+iccan = ICCAN()
+design = iccan.generate(color, texture, pattern)
+
+# Return generated design as JSON response
+return jsonify(design)
+
+if __name__ == '__main__':
+app.run()
