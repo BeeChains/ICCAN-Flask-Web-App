dd159fa83519cb0d51cb8562fbbe562e4d7d8573
xn--tbdhns/ | git.༕༞༑༙ <37987346+BeeChains@users.noreply.github.com>
2023-07-07 15:20:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate():
-# Get parameters from request
-color = request.form['color']
-texture = request.form['texture']
-pattern = request.form['pattern']
+    # Get parameters from request
+    color = request.form['color']
+    texture = request.form['texture']
+    pattern = request.form['pattern']
 
-# Generate camouflage design
-iccan = ICCAN()
-design = iccan.generate(color, texture, pattern)
+    # Generate camouflage design
+    iccan = ICCAN()
+    design = iccan.generate(color, texture, pattern)
 
-# Return generated design as JSON response
-return jsonify(design)
+    # Return generated design as JSON response
+    return jsonify(design)
 
 if __name__ == '__main__':
 app.run()
